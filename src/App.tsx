9b307bbf4e05ef7c7d5193662b285db939bb40c5
Cameron Burrows <cameronb@outlook.com.au>
2021-09-17 15:51:43 +1000

    remove fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { hot } from 'react-hot-loader/root'
+import Home from './views/Home'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact>
-          <div className="w-full flex justify-center">
-            <h1 className="text-2xl">Hello World</h1>
-          </div>
-        </Route>
+        <Route path="/" exact component={Home} />
       </Switch>
     </Router>
   )
---
+++ src/views/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {}
+
+const Home = (props: Props) => {
+  return (
+    <div className="w-full flex justify-center">
+      <h1 className="text-2xl">Hello World</h1>
+    </div>
+  )
+}
+
+export default Home
